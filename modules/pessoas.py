238ae50f1ea70798b7cfd1d09db6e8accb6e7984
VonStoffen <126831234+VonStoffen@users.noreply.github.com>
2025-05-28 16:04:02 -0300

    Add files via upload

--- modules/pessoas.py
+++ modules/pessoas.py
@@ -1,5 +1,9 @@
-import json
-from dataclasses import asdict
+'''
+1 - Criar uma classe chamada: Pessoa
+    1.1 - Precisa ter uma caracteristicas: Peso, Altura, Nome, Idade
+* Ter a classe pessoa para criar uma lista de pessoas usando a classe
+'''
+
 class Pessoa:
     
     def __init__(self) -> None:
@@ -8,7 +12,7 @@ class Pessoa:
         self.altura: float = 0.
         self.peso: float = 0.
         self.lista_pessoas = []
-          
+
     def adicionar_pessoa(self, nome: str, idade: int, altura: float, peso: float) -> None:
         self.nome = nome
         self.idade = idade
@@ -18,10 +22,8 @@ class Pessoa:
         self.lista_pessoas.append(pessoa)
 
     def listar_pessoas(self) -> None:
-        print(f'{self.lista_pessoas}')
-
-    def __str__(self) -> str:
-       return f'{self.nome}, {self.idade}, {self.altura}, {self.peso}'
+        for item in self.lista_pessoas:
+            print(item['nome'], item['idade'], item['altura'],item['peso'])
     
     def achar_pessoa(self, nome) -> None:
         for i, item in enumerate(self.lista_pessoas):
@@ -30,29 +32,38 @@ class Pessoa:
                 break
         else:
             print(f'{nome} não achado na lista')
+    def listar_nome_peso(self):
+        for item in self.lista_pessoas:
+            print("Nome: ",item['nome'],"Peso: ",item['peso'])
 
-    def validar_pessoa(self, nome: str, idade: float, altura: float, peso: float) -> bool:
-        #Dispare erros apropriados (ValueError) com mensagens claras:
-        if idade < 0 or altura < 0 or peso < 0:
-            raise ValueError('Idade, altura e peso não podem ser negativos')
-        if not nome:
-            raise ValueError('Nome não pode ser vazio')
-        return True
+    def trocar_idade(self, nome, idade):
+        for item in self.lista_pessoas:
+            if item['nome'] == nome:
+                item['idade'] = idade
+                print('Idade alterada com sucesso...')
+                print(item['nome'], 'Nova idade', item['idade'])
+                break
+        else:
+            print(f'{nome} não achado na lista')
+
+    def retirar_pessoas(self, nome):
+        for item in(self.lista_pessoas):
+            if item['nome'] == nome:
+                self.lista_pessoas.remove(item)
+                print(f'Registro da pessoa {nome} removido com sucesso...')
+                #print(item['nome'], item['idade'])
+                break
+        else:
+            print(f'{nome} não achado na lista')
+    def ordenar_idade(self, ordem: bool=False):
+        '''Órdem True e ordem Descendente e False e Ascendente'''
+        for item in(self.lista_pessoas):
+            idade = item['idade']
+            pessoas_ordenada = sorted(idade,reverse=ordem)
+            print(pessoas_ordenadas)
+
+
+
+            
+
     
-    def salvar_em_arquivo(self, nome_arquivo: str) -> None:
-            try:
-                with open(nome_arquivo, 'x') as arquivo:
-                    json.dump(self.lista_pessoas, arquivo, indent=4)
-            except Exception as e:
-                print(f'Erro ao salvar arquivo: {e}')
-
-    def carregar_de_arquivo(self, nome_arquivo: str) -> None:
-            with open(nome_arquivo, 'r') as arquivo:
-                self.lista_pessoas = json.load(arquivo)]
-
-    def listar_pessoas_ordenadas_por_idade(self) -> None:
-        pessoas_ordenadas = sorted(self.lista_pessoas, key=lambda x: x['idade'])
-        print(f'Pessoas ordenadas por idade: {pessoas_ordenadas}')
-    def filtrar_pessoas_por_idade(self, idade_minima: int) -> None:
-        pessoas_filtradas = [pessoa for pessoa in self.lista_pessoas if pessoa['idade'] > idade_minima]
-        print(f'Pessoas com idade maior que {idade_minima}: {pessoas_filtradas}')
